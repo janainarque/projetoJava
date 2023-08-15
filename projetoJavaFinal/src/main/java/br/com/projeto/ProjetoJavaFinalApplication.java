94d7ce1f56f7e5ff5bfe753789d7a78178fa6965
Janaina Arque <janainarque@gmail.com>
2023-08-14 21:20:53 -0300

    projeto final de java

---
+++ projetoJavaFinal/src/main/java/br/com/projeto/ProjetoJavaFinalApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaFinalApplication.class, args);
+	}
+
+}
